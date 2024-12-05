0d22725d2d819946de99d15aacf49756a9cc582c
Eric Moore <ericmoore0709@gmail.com>
2024-12-05 14:29:57 -0800

    Aded NotFound404 component

---
+++ recipes_frontend/src/components/util/NotFound404.jsx
@@ -0,0 +1,9 @@
+const NotFound404 = () => {
+    return (
+        <div className="d-flex flex-column align-items-center">
+            <h2>Not Found</h2>
+        </div>
+    );
+}
+
+export default NotFound404;
