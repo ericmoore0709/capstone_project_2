90f5b37554e0eb04a4ac898036ca10ff3b81ce00
Eric Moore <ericmoore0709@gmail.com>
2024-12-11 14:54:20 -0800

    Fixed backend token verification

--- recipes_backend/config.js
+++ recipes_backend/config.js
@@ -3,7 +3,7 @@
 require("dotenv").config();
 require("colors");
 
-const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+const SECRET_KEY = process.env.JWT_SECRET || "secret-dev";
 
 const PORT = +process.env.PORT || 3001;
 
