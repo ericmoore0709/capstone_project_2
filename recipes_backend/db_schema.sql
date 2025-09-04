6a72f5bb1bcd034e57ea728a1f86f8ffe6c25b68
ericmoore0709 <159437312+ericmoore0709@users.noreply.github.com>
2025-09-04 00:59:43 +0000

    added indexes to improve database performance

--- recipes_backend/db_schema.sql
+++ recipes_backend/db_schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    google_id VARCHAR(100) NOT NULL,
+    google_id VARCHAR(100) NOT NULL UNIQUE,
     image VARCHAR(255) DEFAULT NULL,
     deleted_at TIMESTAMP DEFAULT NULL
 );
@@ -49,6 +49,7 @@ CREATE TABLE recipe_tags (
     id SERIAL PRIMARY KEY,
     recipe_id INTEGER NOT NULL REFERENCES recipes(id),
     tag_id INTEGER NOT NULL REFERENCES tags(id)
+    UNIQUE(recipe_id, tag_id)
 );
 
 -- Shelves table
@@ -65,12 +66,13 @@ CREATE TABLE shelf_recipes (
     id SERIAL PRIMARY KEY,
     shelf_id INTEGER NOT NULL REFERENCES shelves(id),
     recipe_id INTEGER NOT NULL REFERENCES recipes(id)
+    UNIQUE(shelf_id, recipe_id)
 );
 
 -- profiles table
 CREATE TABLE profiles (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES users(id),
+    user_id INTEGER NOT NULL UNIQUE REFERENCES users(id),
     bio VARCHAR(250) DEFAULT NULL
 );
 
@@ -81,8 +83,20 @@ CREATE TABLE communities (
     description TEXT DEFAULT NULL,
     admin_id INTEGER NOT NULL REFERENCES users(id),
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    last_updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    last_updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP DEFAULT NULL
 );
+
+-- indeces
+CREATE INDEX idx_recipes_author_id ON recipes(author_id);
+CREATE INDEX idx_recipes_visibility_id ON recipes(visibility_id);
+CREATE INDEX idx_recipes_deleted_at ON recipes(deleted_at);
+CREATE INDEX idx_ingredients_recipe_id ON ingredients(recipe_id);
+CREATE INDEX idx_shelfrecipes_shelf_id ON shelf_recipes(shelf_id);
+CREATE INDEX idx_shelves_user_id ON shelves(user_id);
+CREATE INDEX idx_shelves_deleted_at ON shelves(deleted_at);
+CREATE INDEX idx_communities_admin_id ON communities(admin_id);
+CREATE INDEX idx_communities_deleted_at ON communities(deleted_at);
 
 -- Trigger function to auto-update last_updated_at on update
 CREATE OR REPLACE FUNCTION update_last_updated_column()
